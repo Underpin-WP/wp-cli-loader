ac04506dd194f61fa85b1ef5abd1f9ca550cd6da
Alex Standiford <a@alexstandiford.com>
2021-11-21 19:03:45 -0600

    Compat for Underpin 2.0.0

--- cli.php
+++ cli.php
@@ -8,15 +8,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $file, $class ) {
-
-	// Only register these items if debug mode is enabled, or if WP CLI is running.
-	if ( ( defined( 'WP_CLI' ) && WP_CLI ) || Underpin::is_debug_mode_enabled() ) {
-		require_once( plugin_dir_path( __FILE__ ) . 'Command.php' );
-		require_once( plugin_dir_path( __FILE__ ) . 'Command_Instance.php' );
-		underpin()->get( $file, $class )->loaders()->add( 'cli', [
-			'instance' => 'Underpin_Commands\Abstracts\Command',
-			'default'  => 'Underpin_Commands\Factories\Command_Instance',
-		] );
-	}
-}, 4, 2 );
+Underpin::attach( 'setup', new \Underpin\Factories\Observer( 'cli_command', [
+	'update' => function ( Underpin $plugin ) {
+		// Only register these items if debug mode is enabled, or if WP CLI is running.
+		if ( ( defined( 'WP_CLI' ) && WP_CLI ) || Underpin::is_debug_mode_enabled() ) {
+			require_once( plugin_dir_path( __FILE__ ) . 'Command.php' );
+			require_once( plugin_dir_path( __FILE__ ) . 'Command_Instance.php' );
+			$plugin->loaders()->add( 'cli', [
+				'instance' => 'Underpin_Commands\Abstracts\Command',
+				'default'  => 'Underpin_Commands\Factories\Command_Instance',
+			] );
+		}
+	},
+] ) );
